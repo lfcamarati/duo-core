5b63179b395c428669179c06488aad98c7b5fb29
Lucas Câmara <lfcamara.ti@gmail.com>
2022-10-22 15:00:42 -0300

    remove wrong reference to entity

--- domain/clientpj/infra/repository/clientpj_mysql_repository.go
+++ domain/clientpj/infra/repository/clientpj_mysql_repository.go
@@ -4,8 +4,6 @@ import (
 	"database/sql"
 
 	_ "github.com/go-sql-driver/mysql"
-	client "github.com/lfcamarati/duo-core/domain/client/entity"
-	clientRepository "github.com/lfcamarati/duo-core/domain/client/infra/repository"
 	"github.com/lfcamarati/duo-core/domain/clientpj/entity"
 )
 
@@ -18,19 +16,19 @@ type ClientPjMysqlRepository struct {
 }
 
 func (repository ClientPjMysqlRepository) Save(clientpj entity.ClientPj) (*int64, error) {
-	client := client.Client{
-		Address: clientpj.Address,
-		Email:   clientpj.Email,
-		Phone:   clientpj.Phone,
-		Type:    clientpj.Type,
-	}
-
-	clientRepo := clientRepository.NewClientMysqlRepository(repository.Tx)
-	id, err := clientRepo.Save(client)
+	stmt, err := repository.Tx.Prepare("INSERT INTO client (address, email, phone, type) VALUES (?, ?, ?, ?)")
 
 	if err != nil {
 		return nil, err
 	}
+
+	rs, err := stmt.Exec(clientpj.Address, clientpj.Email, clientpj.Phone, clientpj.Type)
+
+	if err != nil {
+		return nil, err
+	}
+
+	id, err := rs.LastInsertId()
 
 	if err != nil {
 		return nil, err
@@ -48,7 +46,7 @@ func (repository ClientPjMysqlRepository) Save(clientpj entity.ClientPj) (*int64
 		return nil, err
 	}
 
-	return id, nil
+	return &id, nil
 }
 
 func (repository ClientPjMysqlRepository) GetAll() ([]entity.ClientPj, error) {
@@ -119,8 +117,7 @@ func (repository ClientPjMysqlRepository) Delete(id int64) error {
 		return err
 	}
 
-	clientRepo := clientRepository.NewClientMysqlRepository(repository.Tx)
-	err = clientRepo.Delete(id)
+	_, err = repository.Tx.Exec("DELETE FROM client WHERE id = ?", id)
 
 	if err != nil {
 		return err
